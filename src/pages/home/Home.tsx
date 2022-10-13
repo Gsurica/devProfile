bc8c57ef4e40c84d2b9a07a16b78a47a7ccfd7c3
gsurica <eosurica@gmail.com>
2022-10-13 20:21:55 -0300

    adding home components

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container, Title } from './styles';
+
+export const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home component!</Title>
+    </Container>
+  );
+};
